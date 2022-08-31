1f3584a4ba47e2ac21ad2b3c89a6d543ddbb04f8
DDepGS <76416845+DDepGS@users.noreply.github.com>
2022-08-30 22:28:45 -0500

    Add files via upload

--- Awitaditto2/app/src/main/java/com/example/awitaditto/MainActivity.java
+++ Awitaditto2/app/src/main/java/com/example/awitaditto/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.awitaditto;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void TipoUsuarioRegistro(View view) {
+            // Is the button now checked?
+            boolean checked = ((RadioButton) view).isChecked();
+
+            // Check which radio button was clicked
+            switch(view.getId()) {
+                case R.id.PacienteIn:
+                    if (checked)
+                        // Pirates are the best
+                        break;
+                case R.id.EspecialistaIn:
+                    if (checked)
+                        // Ninjas rule
+                        break;
+            }
+        }
 }
